e20ecf84c4cdcc37f50155a1b2a3ad02c7138ecb
A.Shashi.Chaurasia <A.Shashi.Chaurasia@PDC1-4S7V7R1.GDNINDIA.COM>
2012-12-10 17:24:20 +0530

    Adding A New Maven Project in GitHub.

---
+++ app-core/src/main/java/com/acc/test/cq/HelloPerson.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.acc.test.cq;
+
+import com.day.cq.wcm.api.Page;
+
+/**
+ * @author A.Shashi.Chaurasia
+ * 
+ */
+public class HelloPerson {
+
+	private Page personPage;
+	public static final String PN_PERSON_NAME = "personName";
+
+	public HelloPerson(Page personPage) {
+		this.personPage = personPage;
+	}
+
+	public String getHelloMessage() {
+		String personName = "";
+		if(personPage.getProperties().get(PN_PERSON_NAME) != null){
+			personName = personPage.getProperties().get(PN_PERSON_NAME)
+			.toString();	
+		}else{
+			personName ="--empty--";
+		}
+		return personName;
+	}
+
+}
---
+++ app-core/src/main/java/com/cq5/myapp/app_core/App.java
@@ -0,0 +1,13 @@
+package com.cq5.myapp.app_core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
